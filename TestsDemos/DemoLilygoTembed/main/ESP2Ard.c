6f83a222140fb3169e657b29da82b373ed378a9d
Blake Hannaford <blake.hannaford@gmail.com>
2023-10-10 14:19:23 -0700

    adding project files for DemoLilygoTembed

---
+++ TestsDemos/DemoLilygoTembed/main/ESP2Ard.c
@@ -0,0 +1,288 @@
+/*
+ *  ESP32 to Arduino Communication Package
+ *      Blake Hannaford   10/23
+ * Note:
+ *    for Arduino file must be named ESP2Ard.cpp
+ *    for ESP IDF environment file must be named ESP2Ard.c
+ *    ... BUT the file can be identical -- this file
+ */
+
+#include "ESP2Ard.h"
+
+//
+// Set up for platform and serial environment
+#ifdef ARDUINO_PLATFORM
+#include <Arduino.h>
+#endif
+#ifdef ESP32_PLATFORM
+#endif
+
+//
+//   Serial functions for ARDUINO_SW_SERIAL
+//
+#ifdef ARDUINO_SW_SERIAL
+#include <SoftwareSerial.h>
+SoftwareSerial* gSwSerialptr;  // place to setup SwSerial object
+
+//ARDUINO_SW_SERIAL
+void EA_init_serial(int rcv, int tx){
+    static SoftwareSerial sws(rcv,tx);
+    sws.begin(ESP2Ard_BaudRate);
+    EA_log("... sw serial initialized ");
+    gSwSerialptr = &sws;
+    return NULL ;
+  }
+
+//ARDUINO_SW_SERIAL
+int EA_available(){
+    int x = gSwSerialptr->available();
+    return x;
+  }
+
+//ARDUINO_SW_SERIAL
+char EA_read(){
+    return gSwSerialptr->read();
+    }
+
+int EA_write(){
+  return 0;   //TODO: implement arduino send
+}
+#endif
+
+
+//
+//     Serial functions for ESP32 IDF IDE
+//
+#ifdef ESP32_HW_SERIAL
+
+#include "driver/uart.h"
+#include "soc/uart_struct.h"
+
+#define BUF_SIZE (int) (ESP32Ard_max_packet_size * 2)
+
+QueueHandle_t uart_queue; // RTOS queue for UART
+
+uart_config_t* uartConfig;   //  keep config around
+int pin_rcv = ESP_PIN_RX;
+int pin_tx  = ESP_PIN_TX;
+
+
+// ESP32_HW_SERIAL
+void EA_init_serial(int rcv, int tx){
+    uart_config_t uart_config = {
+        .baud_rate = ESP2Ard_BaudRate,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,    //UART_HW_FLOWCTRL_CTS_RTS,
+        .rx_flow_ctrl_thresh = 122,
+    };
+    uartConfig = &uart_config;
+    pin_rcv = rcv;
+    pin_tx  = tx;
+
+    //  create RTOS queue for serial data
+    xQueueCreate( ESP32Ard_max_packet_size*2,   1 );
+
+    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_2, BUF_SIZE * 2, BUF_SIZE * 2, 10, &uart_queue, 0));
+    ESP_ERROR_CHECK(uart_param_config(UART_NUM_2, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UART_NUM_2, PIN_TX, PIN_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
+    ESP_LOGI(TAG,"ESP serial port 2 initialized");
+    }
+
+
+// ESP32_HW_SERIAL
+int EA_available(){
+  ESP_LOGI(TAG, "ESP2Ard error:  EA_available() not allowed on ESP_HW_SERIAL platform. use EA_read()");
+  // die here...
+  while(1)  EA_delay_ms(2000);
+  }
+
+// ESP32_HW_SERIAL
+//  read one byte if available.
+char EA_read(){
+    EA_msg_byte buf[3]={'\0'};
+#define NUM_BYTES 1 // for compatibility w/ Arduino code
+    int n =  uart_read_bytes(ESP_UART_NUM, buf, NUM_BYTES, pdMS_TO_TICKS(ESP32Ard_timeout_delay_ms));
+    if (n< 0) n=0;
+    return n;
+    }
+
+// ESP32_HW_SERIAL
+int EA_write_pkt_serial(EA_msg_byte* buf, int len){
+  int code = EA_test_packet(buf);
+  if (code == ESP32Ard_packet_check_OK){
+    EA_log("packet checked out.. sending");
+    uart_write_bytes(ESP_UART_NUM, buf, len);
+    return len;   //TODO: implement arduino send
+  }
+  else {
+    char msg[100];
+    EA_log("somethings wrong with uart_write_bytes");
+    sprintf(msg, "EA_write_pkt_serial(): trying to send a bad backet: error: %d",code);
+    EA_log(msg);
+    return 0;
+  }
+}
+
+#endif  // ESP_IDF_PLATFORM
+
+
+//  Platform agnostic functions
+
+void EA_delay_ms(int dms){
+#ifdef ARDUINO_PLATFORM
+    delay(dms); // ms
+#endif
+#ifdef ESP32_IDF_PLATFORM
+    vTaskDelay(pdMS_TO_TICKS(dms));
+    // 1ms Free/RTOS delay
+#endif
+}
+
+int EA_get_packet_serial(EA_msg_byte* buf){
+  int pidx = 0;
+  pidx = 0;
+  int timeoutms = ESP32Ard_timeout_delay_ms;
+  while(1){
+    timeoutms--;
+    if (EA_available()  == 0 )  return 0;
+   // Serial.print("\n");
+    while (EA_available() > 0) {
+        buf[pidx] = EA_read();
+#ifdef ARDUINO_PLATFORM
+#ifdef ESP2Ard_DEBUG
+   //   Serial.println( sprintf(">> %d, %c", pidx, packet[pidx]) );
+        Serial.print(">> ");
+        Serial.print(pidx);
+        Serial.print("  ");
+        Serial.print((EA_msg_byte) buf[pidx], HEX);
+        Serial.println("");
+#endif
+#endif
+        pidx++;
+        if (buf[pidx] == '\n')  break; // == 0xA
+      EA_delay_ms(1);
+    }
+    if (timeoutms <= 0) { pidx = 0; break;}
+    else {
+      timeoutms = ESP32Ard_timeout_delay_ms; // reset timout timer
+      return pidx;  // num of bytes read
+      }
+  }
+  return ESP32Ard_timeout_error;  // approx timeoutms ms.
+}
+
+// build a packet given a payload
+int EA_pkt_build(EA_msg_byte* pkt, int payload_len, EA_msg_byte* payload){
+  pkt[0] = 0xFF;  // packet header0
+  pkt[1] = 0x00;  // packet header1
+  if (payload_len > 255) {
+    EA_log("payload must be 255 bytes or less");
+    while(1) EA_delay_ms(2000);  //freeze here.
+  }
+  pkt[2] = (char) payload_len; // payload byte count
+  byte cksum = 0;
+  int end_of_payload = 3;
+  for (int i=0;i<payload_len;i++){ // copy payload into packet
+    pkt[3+i] = payload[i];
+    cksum += payload[i];
+    end_of_payload++;
+  }
+  if (end_of_payload > ESP32Ard_max_packet_size){
+    EA_log("packet is too big for local buffers: increase ESP32Ard_max_packet_size");
+    while(1)  EA_delay_ms(2000);
+    }
+  pkt[end_of_payload] = cksum;
+  pkt[end_of_payload+1] = '\n';  // end of packet flag
+  int pktLen = payload_len + ESP2Ard_OVERHEAD_BYTES;
+  return pktLen; // total pkt length
+  }
+
+void EA_dump_packet_bytes(EA_msg_byte* pkt){
+    // print it out raw for user
+#ifdef ARDUINO_PLATFORM
+    Serial.println(">> packet bytes (hex): ");
+    Serial.println("-------");
+    for (int i=0;;i++){
+      Serial.print(" [");
+      Serial.print( (EA_msg_byte) pkt[i], HEX);
+      Serial.print("] ");
+      if(pkt[i] ==  '\n') break;
+      if(i>ESP32Ard_max_packet_size) break;
+      }
+    Serial.println("");
+    Serial.println("-------");
+#endif
+#ifdef ESP32_IDF_PLATFORM
+    ESP_LOGI(TAG,">> packet bytes (hex): ");
+    ESP_LOGI(TAG,"--------");
+    for (int i=0;;i++){
+      ESP_LOGI(TAG,"%2d [0x%02x]",i,pkt[i]);
+      if(pkt[i] ==  '\n') break;
+      if(i>ESP32Ard_max_packet_size) break;
+      }
+    ESP_LOGI(TAG,"-------");
+#endif
+}
+//
+//  log method for functions that work on all platforms
+//
+void EA_log(const char* msg){
+#ifdef  ESP32_IDF_PLATFORM
+  printf(msg);
+#endif
+#ifdef ARDUINO_PLATFORM
+  Serial.println(msg);
+#endif
+}
+
+int EA_test_packet(EA_msg_byte* pkt){
+#ifdef ARDUINO_PLATFORM
+#ifdef ESP2Ard_DEBUG
+  Serial.println(" .. test a packet ...");
+#endif
+#endif
+  if (pkt[0] != 0xFF && pkt[1] != 0)
+    return ESP32Ard_bad_pkt_header;
+  int pkt_payload_size = (int)pkt[2];
+  if (pkt_payload_size == 0)                return ESP32Ard_payload_size_zero;
+  int len_payload=0;
+  int len_packet=0;
+  byte rcksum = 0;
+  //
+  //  compute length and payload checksum
+  //
+  for (int i=0; i<ESP32Ard_max_packet_size ; i++){
+    len_packet++;
+    if (pkt[i] == 0xA) break;
+    if (i>= ESP32Ard_max_packet_size)  return ESP32Ard_packet_length_overrun;
+    }
+    len_payload = len_packet-5;
+    // get payload checksum
+    for (int i=0; i<len_payload; i++){ // actually add up the payload
+      rcksum += pkt[i+3];
+    }
+  int pkt_cksum_idx = len_payload+3;
+#ifdef ARDUINO_PLATFORM
+#ifdef ESP2Ard_DEBUG
+  Serial.print("  1>");
+  Serial.print(len_payload);
+  Serial.print("  2>");
+  Serial.print(len_packet);
+  Serial.print("  3>");
+  Serial.print(rcksum);
+  Serial.print("  >");
+
+  Serial.println("");
+  Serial.print("cksum test: ");
+  Serial.print((int)rcksum);
+  Serial.print("  <");
+  Serial.print((int) pkt[pkt_cksum_idx]);
+  Serial.println(">");
+#endif
+#endif
+  if (rcksum != (byte) pkt[pkt_cksum_idx])            return ESP32Ard_packet_cksum_error;
+  if (len_payload != pkt_payload_size)      return ESP32Ard_packet_length_incorrect;
+  return ESP32Ard_packet_check_OK;
+}
---
+++ TestsDemos/DemoLilygoTembed/main/ESP2Ard.h
@@ -0,0 +1,119 @@
+/*
+ *    ESP2Ard.h :
+ *
+ *          ESP32 to Arduino serial port (UART) communication
+ *
+ *  A basic library for sending simple packets back and forth
+ *
+ * Packet format: (8-bit bytes)
+ * [  0]   0xff      Start Flag 1
+ * [  1]   0x00      Start Flag 2
+ * [  2]   (byte)L   number of payload bytes
+ * [  3]   ...       start of payload
+ * [3+L]  cksum      Checksum of payload (%256)
+ * [4+L]  '\n'       End of packet flag
+ *
+ * L = # of payload bytes
+ *   (1 <= L <= 255)
+ *
+ * Packet total length:   ESP2Ard_OVERHEAD_BYTES + L bytes
+ */
+
+//  Debugging?
+#define ESP2Ard_DEBUG   1
+
+
+/////////////////////////  Configuration Zone ///////////////////////////////////////////
+//  ESP2Ard_DEBUG works only on Arduino for now
+//#define ESP2Ard_DEBUG  // more verbose debugging output
+
+////////////////////////////////////////////
+//
+//  What platform and IDE are we running on?
+//    uncomment just one of these:
+
+// #define ARDUINO_PLATFORM            // arduino HW on Arduino IDE
+#define ESP32_IDF_PLATFORM        // ESP32 HW on IDF IDE
+//#define ESP32_Arduino_PLATFORM    // ESP32 HW on Arduino IDE
+
+
+//
+//  All platforms:
+//
+//   Configure comm parameters which should be same on both ends
+//
+#define ESP32Ard_max_packet_size         50
+#define ESP32Ard_timeout_delay_ms        50
+#define ESP2Ard_BaudRate               9600
+#define ESP2Ard_OVERHEAD_BYTES            5  //len(packet) - len(payload)
+#define ESP2Ard_max_payload_size  ESP32Ard_max_packet_size -  ESP2Ard_OVERHEAD_BYTES
+
+//
+//  >>> ESP32, configure serial params here
+//
+#define ESP_PIN_RX              16  // Receive data pin on ESP32
+#define ESP_PIN_TX              17
+#define ESP_UART_NUM    UART_NUM_2  //UART_NUM_2 from driver/uart.h
+
+#ifdef ARDUINO_PLATFORM
+//
+//   Arduino pin setup
+#define ARD_PIN_RX   2
+#define ARD_PIN_TX   3
+
+//  Arduino Serial port type
+#define ARDUINO_SW_SERIAL
+//#define ARDUINO_HW_SERIAL
+
+#endif // ARDUINO_PLATFORM
+
+/////////////////////////////////  End of Configuration Zone ////////////////////////////
+
+////////////////////////////////////////////
+//  types and function protos
+
+#ifdef ESP32_IDF_PLATFORM
+#include "esp_log.h"
+#include "driver/uart.h"
+
+#define ESP32_HW_SERIAL
+
+
+#define TAG "ESP2Ard message:"  // used for logging
+
+typedef   unsigned char  byte;  // used w/ arduino just in case
+typedef   unsigned char  EA_msg_byte;
+
+#endif  // ESP32_IDF_PLATFORM
+
+#ifdef ARDUINO_PLATFORM
+typedef   byte  EA_msg_byte;
+#endif
+//
+// defines for all platforms and ports:
+//
+
+// Error codes
+#define ESP32Ard_timeout_error           -1
+#define ESP32Ard_bad_pkt_header          -2
+#define ESP32Ard_packet_length_overrun   -3
+#define ESP32Ard_packet_length_incorrect -4
+#define ESP32Ard_payload_size_zero       -5
+#define ESP32Ard_packet_cksum_error      -6
+
+#define ESP32Ard_packet_check_OK          1
+
+//
+//   Our basic coms functions (all platforms)
+//
+void EA_log(const char* msg);
+int  EA_available();
+void EA_delay_ms(int dms);
+char EA_read();
+char EA_write_buffer(EA_msg_byte* buf, int len);
+int  EA_get_packet_serial(EA_msg_byte* buf);
+int  EA_test_packet(EA_msg_byte* pkt);
+int  EA_pkt_build(EA_msg_byte* pkt,int pll, EA_msg_byte* payload);
+int  EA_write_pkt_serial(EA_msg_byte* pkt, int len);
+void EA_init_serial(int rcv, int tx);
+void EA_dump_packet_bytes(EA_msg_byte *pkt);
