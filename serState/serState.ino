80684b99286c77c527e5ec3e0e457133768a71be
Blake Hannaford <blake.hannaford@gmail.com>
2023-10-27 12:52:08 -0700

    pkt receive state machine can now be debugged on PC w/ gcc

--- serState/serState.ino
+++ serState/serState.ino
@@ -2,18 +2,62 @@
        State machine for receiving serial packets (ESP2Ard)
 */
 
+// #define  ArduinoAIDE    // for uploading to Arduino
+#define  GCC            // run locally with   >gcc -o ss serState.c
+
+
+
+//  need ths for ESP2Ard.h
 #define EA_timeout_ERROR  -10
+
+int state_machine(char , char* );
+
+#ifdef GCC
+  // just for stubs
+  #define LED_BUILTIN  0
+  #define OUTPUT 0
+  #define HIGH   1
+  #define LOW    0
+  #define HEX    1
+
+  #include <stdarg.h>
+  #include <unistd.h>
+  void delay(int n);
+  void SerialDOTbegin(int baudrate);
+  void SerialDOTprint(char* txt);
+  void SerialDOTprintln(char* txt);
+  void pinMode(int a1, int a2);
+  void digitalWrite(int a1, int a2);
+  int EA_read(char* cdata_ptr); // return 1=valid data byte, 0 = no databyte
+
+#endif
+
+//
+//   Code for both
+//
+
+#include <stdio.h>
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+#ifdef ArduinoAIDE
   Serial.begin(9600);
+#endif
+#ifdef GCC
+  SerialDOTbegin(9600);
+#endif
   delay(2000);
+#ifdef ArduinoAIDE
   Serial.println("       Starting  Serial StateMachine Test  \n\n");
+#endif
+#ifdef GCC
+  SerialDOTprintln("       Starting  Serial StateMachine Test  \n\n");
+#endif
 }
 
-int rcvpkt(char buf[], int timeout){
+int rcvpkt(char* buf, int timeout){
   int tout = timeout;
   int ptr = 0;
   int smreturn = 0;
@@ -26,7 +70,7 @@ int rcvpkt(char buf[], int timeout){
     if (smreturn > 0) { // we got a full packet
         return smreturn;
         }
-//    Serial.print(" in state: ");Serial.print(smreturn);Serial.print("  char2: ");Serial.println(c,HEX);
+//    SerialDOTprint(" in state: ");SerialDOTprint(smreturn);SerialDOTprint("  char2: ");SerialDOTprintln(c,HEX);
     delay(1);
     tout--;
     if (tout < 0) return EA_timeout_ERROR;
@@ -101,20 +145,42 @@ int EA_read(char* cdata_ptr){ // return 1=valid data byte, 0 = no databyte
   if (dropcnt++ % dropinterval != 0){
     // print the bytes as they are "read"
     if (i<=len)  {
-      Serial.print(" (simread)>");Serial.println((char)tbytes[i], HEX);
+
+#ifdef ArduinoAIDE
+      Serial.print(" (simread)>");Serial.println(&tbytes[i]);
+#endif
+#ifdef GCC
+      SerialDOTprint(" (simread)>");SerialDOTprintln(&tbytes[i]);
+#endif
+
       *cdata_ptr = tbytes[i++];
       return 1; }
     else {
+
+#ifdef ArduinoAIDE
       Serial.println("ERROR: test bytes overrun");
+#endif
+#ifdef GCC
+      SerialDOTprintln("ERROR: test bytes overrun");
+#endif
       while(1) delay(100);
       return -500;}
     }
   else {  // simulate no bytes available
+
+
+#ifdef ArduinoAIDE
     Serial.println("sim. drop");
-    *cdata_ptr = NULL;
+#endif
+#ifdef GCC
+    SerialDOTprintln("sim. drop");
+#endif
+
+    cdata_ptr = NULL;
     return 0;
   }
 }
+
 // the loop function runs over and over again forever
 void loop() {
   int retval = 0;
@@ -125,8 +191,52 @@ void loop() {
 
   // Test rcvpkt
   static char buffer[200]={0};
-  Serial.println("begining the test:");
   retval = rcvpkt(buffer, 50);
-  if (retval>0){  Serial.println("Complete packet Returned (subject to EA_test_pkt() verification"); while(1) delay(500);}
-  if (retval == EA_timeout_ERROR) { Serial.println("ERROR: packet read timeout"); while(1) delay(500);}
+  if (retval>0){
+#ifdef ArduinoAIDE
+    Serial.println("Complete packet Returned (subject to EA_test_pkt() verification");
+#endif
+#ifdef GCC
+    SerialDOTprintln("Complete packet Returned (subject to EA_test_pkt() verification");
+#endif
+    while(1) delay(500);
+    }
+  if (retval == EA_timeout_ERROR) {
+#ifdef ArduinoAIDE
+    Serial.println("ERROR: packet read timeout");
+#endif
+#ifdef GCC
+    SerialDOTprintln("ERROR: packet read timeout");
+#endif
+    while(1) delay(500);}
 }
+
+
+//  Arduino stubs for gcc
+
+#ifdef GCC
+int main(void){
+  loop();
+}
+
+#include <unistd.h>
+void delay(int n){
+  usleep(n*1000);
+}
+
+void SerialDOTbegin(int baudrate){
+}
+void SerialDOTprint(char* txt){
+  printf("%s",txt);
+}
+
+void SerialDOTprintln(char* txt) {
+  printf("%s\n", txt);
+}
+
+void pinMode(int a1, int a2){
+}
+
+void digitalWrite(int a1, int a2){
+}
+#endif
